d573c92a23c0fbfd60f288070cbdefc092d0e0e9
matheus <wallysom2@gmail.com>
2023-11-01 16:16:45 -0300

    feat baseurl

--- src/api/connect.js
+++ src/api/connect.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'https://api-room-78toz7buc-wallysom2.vercel.app/'
 });
 
 export default api;
